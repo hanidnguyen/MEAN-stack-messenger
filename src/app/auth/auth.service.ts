04e11834c316d3512c8bc73fbeb52aa915b7d250
Hani <hani.dnguyen@gmail.com>
2022-10-21 19:34:14 -0700

    Backend auth and middleware to protect routes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { response } from 'express';
 import { AuthData } from './auth-data.model';
 
 
@@ -25,7 +26,17 @@ export class AuthService {
     //Give the route to use in backend
     this.http.post("http://localhost:3000/api/user/signup", authData)
       .subscribe(response => {
-        console.log(response);
+
+      });
+  }
+
+  login(email: string, password: string){
+    const authData: AuthData = {
+      email:email,
+      password: password
+    }
+    this.http.post("http://localhost:3000/api/user/login", authData)
+      .subscribe(response => {
       });
   }
 }
