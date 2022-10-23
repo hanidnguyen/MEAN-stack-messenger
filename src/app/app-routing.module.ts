0425547f5f4331cf4926f8f9904e79e8dabf62a0
Hani <hani.dnguyen@gmail.com>
2022-10-22 18:38:16 -0700

    Redirect user when accessing routes without auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth/auth.guard";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
@@ -7,21 +8,24 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
 
 /**
  * Register all routes and corresponding components here
+ * Also make AuthGuard available for use
+ * Attach canActivate AuthGuard to routes that needs authentication.
  * '' => root page path
  * 'create' => localhost:4200/create
  * : => extract id dynamically
  */
 const routes: Routes = [
   { path: '' , component: PostListComponent },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 
 export class AppRoutingModule {
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,30 @@
+/**
+ * Guard for routes.
+ * Without it, any user can go directly to a /create /edit page.
+ */
+
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+  constructor(private authService: AuthService, private router: Router){}
+
+  /**
+   * @param route
+   * @param state
+   * @returns
+   *
+   * We can rely on isAuth to be current as it updates if we login or logout.
+   * Redirect user to login page when they are not authenticated to reach a specific route.
+   */
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    const isAuth = this.authService.getIsAuth();
+    if(!isAuth){
+      this.router.navigate(['/login']);
+    }
+    return isAuth;
+  }
+}
